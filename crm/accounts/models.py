6ef5a380b560cdecf7a1cad789ffcd41ac422ce3
Gokul <gokulsrinivask2020@gmail.com>
2023-07-17 23:20:16 +0530

    included new model called Tag

--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -11,6 +11,12 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Tag(models.Model):
+    name=models.CharField(max_length=200,null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -19,11 +25,20 @@ class Product(models.Model):
     name = models.CharField(max_length=100,null = True)
     price = models.FloatField(max_length=100 ,null = True)
     category = models.CharField(max_length=100,null =True,choices = CATEGORY)
-    description = models.CharField(max_length=200,null = True)
+    description = models.CharField(max_length=200,null = True,blank=True)
     date_created = models.DateTimeField(auto_now_add=True,null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
 
 class Order(models.Model):
     STATUS = (('Pending','Pending'),('Out for delivery','Out for delivery'),('Delivered','Delivered'),)
     date_created = models.DateTimeField(auto_now_add=True,null=True)
+    cutomer = models.ForeignKey(Customer,null = True,on_delete=models.SET_NULL)
     status = models.CharField(max_length = 200,null = True,choices=STATUS)
+    
+    product = models.ForeignKey(Product,null=True,on_delete=models.SET_NULL)
+    
 
